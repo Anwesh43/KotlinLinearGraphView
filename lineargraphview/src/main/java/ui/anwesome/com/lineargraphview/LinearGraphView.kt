a3123dbcbd55b904587c42ecf37ad74725622e84
Anwesh43 <anweshthecool0@gmail.com>
2018-03-08 08:40:19 +0530

    using Renderer in View

--- lineargraphview/src/main/java/ui/anwesome/com/lineargraphview/LinearGraphView.kt
+++ lineargraphview/src/main/java/ui/anwesome/com/lineargraphview/LinearGraphView.kt
@@ -10,13 +10,14 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 class LinearGraphView(ctx : Context, var y_points : Array<Float>) : View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = Renderer(this)
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
