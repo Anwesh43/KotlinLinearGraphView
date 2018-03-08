2a36248d5ab76eaf0944408347032261bee1dbec
Anwesh43 <anweshthecool0@gmail.com>
2018-03-08 08:02:49 +0530

    adding lineargraphview module

--- lineargraphview/src/main/java/ui/anwesome/com/lineargraphview/LinearGraphView.kt
+++ lineargraphview/src/main/java/ui/anwesome/com/lineargraphview/LinearGraphView.kt
@@ -3,3 +3,21 @@ package ui.anwesome.com.lineargraphview
 /**
  * Created by anweshmishra on 08/03/18.
  */
+import android.content.*
+import android.graphics.*
+import android.view.*
+
+class LinearGraphView(ctx : Context, var y_points : Array<Float>) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
