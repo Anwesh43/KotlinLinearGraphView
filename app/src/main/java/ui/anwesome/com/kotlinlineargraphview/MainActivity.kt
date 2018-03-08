46a0eaf646ec0217f254540d9a94192eb6537472
Anwesh43 <anweshthecool0@gmail.com>
2018-03-08 09:08:29 +0530

    using View in Activity

--- app/src/main/java/ui/anwesome/com/kotlinlineargraphview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinlineargraphview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinlineargraphview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.lineargraphview.LinearGraphView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinearGraphView.create(this, arrayOf(300f,200f, 100f, 1000f, 400f, 600f, 700f, 500f, 900f))
     }
 }
